651b850a41a2f73eb4a36bf003d1324f1ade7813
Márcia Ferreira <marciasilvacf1989@gmail.com>
2025-03-14 16:47:02 -0300

    first SQL queries

---
+++ Postgres.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE ESCOLA;
+
+create table alunos (
+	id_aluno SERIAL PRIMARY KEY,
+	nome varchar(50) not null,
+	email varchar(254)not null,
+	endereco varchar(150) not null
+);
+
+insert into alunos
+(id_aluno, nome, email, endereco)
+values
+('Juliana Gueira','ju@gmail.com','Rua Nobel da Paz, 615, Gorel-SP');
+
+select * from alunos;
